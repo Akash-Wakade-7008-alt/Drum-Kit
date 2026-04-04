7cd6a411bdeead1c62908faf2cac51492c0bac34
Akash-Wakade-7008-alt <akashwakade7008@gmail.com>
2026-04-04 22:52:56 +0530

    improved the code quality

--- script.js
+++ script.js
@@ -4,51 +4,19 @@ for (let i = 0; i < numOfDrumBtn; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
     let btnInnerHTML = this.innerHTML;
 
-    switch (btnInnerHTML) {
-      case "w":
-        let tom1 = new Audio("./sounds/tom-1.mp3");
-        tom1.play();
-        break;
-
-      case "a":
-        let tom2 = new Audio("./sounds/tom-2.mp3");
-        tom2.play();
-        break;
-
-      case "s":
-        let tom3 = new Audio("./sounds/tom-3.mp3");
-        tom3.play();
-        break;
-
-      case "d":
-        let tom4 = new Audio("./sounds/tom-4.mp3");
-        tom4.play();
-        break;
-
-      case "j":
-        let crash = new Audio("./sounds/crash.mp3");
-        crash.play();
-        break;
-
-      case "k":
-        let kickBass = new Audio("./sounds/kick-bass.mp3");
-        kickBass.play();
-        break;
-
-      case "l":
-        let snare = new Audio("./sounds/snare.mp3");
-        snare.play();
-        break;
-
-      default:
-    }
+    instrumentSound(btnInnerHTML);
 
     BtnAnimation(btnInnerHTML);
   });
 }
 
 document.addEventListener("keypress", function (e) {
-  switch (e.key.toLocaleLowerCase()) {
+  instrumentSound(e.key.toLocaleLowerCase());
+  BtnAnimation(e.key.toLocaleLowerCase());
+});
+
+function instrumentSound(keyValue) {
+  switch (keyValue) {
     case "w":
       let tom1 = new Audio("./sounds/tom-1.mp3");
       tom1.play();
@@ -86,9 +54,7 @@ document.addEventListener("keypress", function (e) {
 
     default:
   }
-
-  BtnAnimation(e.key.toLocaleLowerCase());
-});
+}
 
 function BtnAnimation(currentKey) {
   let activeBtn = document.querySelector("." + currentKey);
